b34977b4b749bd5be0b5162d4039ee3befc7e4dc
jian zhang <zhangjian@sys-in.com.cn>
2021-01-10 14:44:10 +0800

    feat: is able to check the price and tickets availability

--- ticket_monitor.py
+++ ticket_monitor.py
@@ -4,12 +4,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox.options import Options
 import time
+
+import bs4
+from bs4 import BeautifulSoup
 
 base_url='http://www.ceair.com/booking/syd-hgh-210205_CNY.html'
 timeout = 6
 
-driver=webdriver.Firefox()
+options = Options()
+options.headless = True
+driver=webdriver.Firefox(options=options)
 driver.get(base_url)
 
 elem_ready_status_check = {
@@ -20,6 +26,7 @@ elem_ready_status_check = {
 elem_located_by_dict = {
     "id": By.ID,
     "class_name": By.CLASS_NAME,
+    "name": By.NAME,
 }
 
 def web_interact(interact):
@@ -27,6 +34,11 @@ def web_interact(interact):
     located_by = interact[1]
     address = interact[2]
     action = interact[3]
+
+    if len(interact) > 4:
+        wait_sec = interact[4]
+        time.sleep(wait_sec)
+        print("wait {} seconds to act".format(wait_sec))
 
     try:
         target = WebDriverWait(driver, timeout).until(
@@ -41,10 +53,31 @@ def web_interact(interact):
     getattr(target, action)()
     print("{} {} executed".format(address, action))
 
+def native_string(tag):
+    for c in tag.children:
+        if isinstance(c, bs4.element.NavigableString):
+            return(c)
+
 interactions = [
     ("clickable", "id", "okay", "click"),
     ("clickable", "id", "okay", "click"),
+    ("clickable", "name", "lowest", "click", 3),
 ]
 
+# Get to the wanted page
 for i in interactions:
     web_interact(i)
+
+# Parsing the wanted page
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+l = soup.find_all('dd', {"name": "lowest"})
+lowest = native_string(l[0])
+print("The lowest price is {}".format(lowest))
+
+k = soup.find("div", {"class": "product-list active"})
+price = k.find_all("dd", {"class": "p-p"})
+for q in price:
+    if lowest == native_string(q):
+        print("match lowest price {}".format(lowest))
+        print(q.next_sibling.find("div", {"class": "bbottom"}).string)
